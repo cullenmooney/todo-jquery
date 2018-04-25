c967e88a05f49705ea93f5d8cba6dd5f89ecc55b
Cullen Mooney <cullen@protonmail.com>
2018-04-25 16:18:29 -0500

    completed and deleted functionality works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("connected");
+// Click on item to complete
+$('li').on("click", function() {
+	$(this).toggleClass("completed");
+});
+
+// Click on X to delete todo
+$('span').on("click", function(e) {
+	// fade out then delete the span's parent li
+	$(this).parent().fadeOut(1000, function() {
+		$(this).remove();
+	})
+	// this event stops it from bubbling up into parent elements
+	e.stopPropagation();
+})
